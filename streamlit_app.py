1a37a82fd94002e23916faa38eafc415c802b68b
Ben Leahy <140358505+Ben-Leahy@users.noreply.github.com>
2026-03-30 11:46:15 +1100

    update streamlit_app.py
    Make connection changes necessary to migrate from SiS to SniS.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Title
 st.title(f":cup_with_straw: Customise Your Smoothie :cup_with_straw:")
