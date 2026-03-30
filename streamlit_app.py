d44bc433aa7eaeb72dfe0710ec2b68e1c598cf0a
Ben Leahy <140358505+Ben-Leahy@users.noreply.github.com>
2026-03-30 12:34:34 +1100

    Update streamlit_app.py
    Add data frame of api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,4 @@ st.dataframe(data=orders_dataframe, use_container_width=True)
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
